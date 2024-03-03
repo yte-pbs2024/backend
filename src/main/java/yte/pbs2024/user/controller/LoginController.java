8d3a230ddd339d4ae451dbcc0a05d60f2d882e26
halid.acar <acarhalid1997@gmail.com>
2024-03-03 14:09:17 +0300

    Authentication update 1.1

--- src/main/java/yte/pbs2024/user/controller/LoginController.java
+++ src/main/java/yte/pbs2024/user/controller/LoginController.java
@@ -2,14 +2,15 @@ package yte.pbs2024.user.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import yte.pbs2024.user.controller.request.LoginRequest;
 import yte.pbs2024.user.service.LoginService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
+//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class LoginController {
 
     private final LoginService loginService;
